771f8d7496e38d2aae4f9c10035bae60c464aa06
Merzaad <Merzaad@gmail.com>
2021-12-30 17:23:13 +0330

    js

--- healthcare/js/hc.js
+++ healthcare/js/hc.js
@@ -1,9 +1,12 @@
 function purchase() {
     let a = "Purchased";
-   
     document.getElementById("purch").innerHTML = a;
+
     let b = document.querySelectorAll('#badge')
     b.forEach(function (value) {
-        value.style.display='none'
-    })
+        value.style.display = 'none'
+    });
+    document.querySelector('#crd').classList.add('border-success')
+    document.querySelector('#crd').classList.add('text-success')
+    document.querySelector('#crdh').classList.replace('text-secondary','text-success')
 }
