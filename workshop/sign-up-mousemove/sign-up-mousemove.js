a2dc88fe1ad39b80e88ab023463eb78182bd4fb3
Merzaad <Merzaad@gmail.com>
2022-01-16 09:12:12 +0330

    mousemove

--- workshop/sign-up-mousemove/sign-up-mousemove.js
+++ workshop/sign-up-mousemove/sign-up-mousemove.js
@@ -1,38 +1,9 @@
 function mouseover(e) {
-    document.querySelectorAll('.planet').forEach((i) => {
+    document.querySelectorAll('.move').forEach((i) => {
         var mv = i.getAttribute("data-value");
-        var x = (e.clientX * mv) / 100;
-        var y = (e.clientY * mv) / 100;
+        var x = (e.clientX * mv) / 200;
+        var y = (e.clientY * mv) / 50;
         i.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
 };
-
-document.addEventListener('mousemove', mouseover);
-
-let a = document.querySelectorAll('.planet');
-
-for (let i in a) {
-    if (i % 2 == 0) {
-        a[i].style = `
-        position:absolute;
-        right: ${Math.random() * 100}%;
-        top:   ${Math.random() * 300}%;
-        z-index: -1;
-        overflow:hidden;
-        width:50px;
-        `;
-    }
-    else {
-        a[i].style = `
-        position:absolute;
-        right: ${Math.random() * 100}%;
-        bottom: ${Math.random() * 100}%;
-        z-index: -1;
-        overflow:hidden;
-        width:50px;
-        `;
-    }
-};
-
-
 document.addEventListener('mousemove', mouseover);
