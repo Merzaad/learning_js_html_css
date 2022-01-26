762df849a8b75f9db85809afa40fff2a5219cc1d
Merzaad <Merzaad@gmail.com>
2022-01-26 12:48:11 +0330

    isLet

--- workshop/sign-up-mousemove/sign-up-mousemove.js
+++ workshop/sign-up-mousemove/sign-up-mousemove.js
@@ -14,11 +14,11 @@ function Validation(event) {
     const lastname = document.forms['signup'][`lastname`];
     const ready = document.forms['signup'][`ready`];
     const code = document.forms['signup'][`code`];
-    const letters = /^[A-Za-z]+$/;
     function isLet(a) {
-        let x = { is: false, errIn: 'nothing' };
+        const letters = /^[A-Za-z]+$/;
+        let x = { is: false, errIn: 'initial' };
         if (a.value.match(letters)) {
-            x.does = true;
+            x.is = true;
             a.style = `border-color:darkgreen`
         }
         else {
@@ -27,14 +27,15 @@ function Validation(event) {
         };
         return x
     };
+    event.preventDefault();
     const isLetName = isLet(name);
     const isLetLastname = isLet(lastname);
-    if (isLet.is && isLet.is) {
+    if (isLetName.is && isLetLastname.is) {
         alert(`Name: ${name.value} , Lastname: ${lastname.value} ,  Ready value: ${ready.value}, Select value: ${code.value}`);
     }
     else {
         event.preventDefault();
-        alert(`name error: ${isLetName.errIn} ----- lastname error: ${isLetName.errIn}`);
+        alert(`name error: ${isLetName.errIn} ----- lastname error: ${isLetLastname.errIn}`);
     }
 };
 document.addEventListener('mousemove', mousemove);
