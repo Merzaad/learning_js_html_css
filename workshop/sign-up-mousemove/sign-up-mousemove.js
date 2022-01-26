5134d8d92ea133ac0a03e4103e66ccae7b9f16a0
Merzaad <Merzaad@gmail.com>
2022-01-26 08:29:36 +0330

    fixed

--- workshop/sign-up-mousemove/sign-up-mousemove.js
+++ workshop/sign-up-mousemove/sign-up-mousemove.js
@@ -14,27 +14,27 @@ function Validation(event) {
     const lastname = document.forms['signup'][`lastname`];
     const ready = document.forms['signup'][`ready`];
     const code = document.forms['signup'][`code`];
-    const letters = /[A-Za-z]+$/;
-    function includesLet(a) {
+    const letters = /^[A-Za-z]+$/;
+    function isLet(a) {
         let x = { is: false, errIn: 'nothing' };
         if (a.value.match(letters)) {
-            x.is = true;
+            x.does = true;
             a.style = `border-color:darkgreen`
         }
         else {
-            x.errIn = `must contain letters`
+            x.errIn = `must be letters`
             a.style = "border-color:darkred";
         };
         return x
     };
-    const includesLetName = includesLet(name);
-    const includesLetLastname = includesLet(lastname);
-    if (includesLetName.is && includesLetLastname.is) {
+    const isLetName = isLet(name);
+    const isLetLastname = isLet(lastname);
+    if (isLet.is && isLet.is) {
         alert(`Name: ${name.value} , Lastname: ${lastname.value} ,  Ready value: ${ready.value}, Select value: ${code.value}`);
     }
     else {
         event.preventDefault();
-        alert(`name error: ${includesLetName.errIn} ----- lastname error: ${includesLetLastname.errIn}`);
+        alert(`name error: ${isLetName.errIn} ----- lastname error: ${isLetName.errIn}`);
     }
 };
 document.addEventListener('mousemove', mousemove);
